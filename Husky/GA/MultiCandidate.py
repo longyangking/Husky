abd749ddc5b3fbb1c684a227f255ae0e8bd4ad75
Long Yang <longyang_123@yeah.net>
2017-03-15 10:59:33 +0800

    Multi-Candidate

--- Husky/GA/MultiCandidate.py
+++ Husky/GA/MultiCandidate.py
@@ -4,8 +4,26 @@
 
 import numpy as np
 
-class Candidates:
-    def __init__(self,popsize,chromesize,func,constraints,IntCon,LB,UB,\
+class Individual:
+    def __init__(self,chromes,targetsize):
+        self.rank = None
+        self.crowdingdistance = None
+        self.dominatedindividuals = list()
+        self.dominatecount = None
+        self.chromes = chromes
+        self.objectives = np.zeros(targetsize)
+
+    def dominate(self,individual):
+        '''
+        To decide whether this individual dominate other individual
+        '''
+        num = np.sum(1*(self.objectives >= individual.objectives))
+        if num == targetsize:
+            return True
+        return False
+
+class MultiCandidates:
+    def __init__(self,popsize,chromesize,func,targetsize,constraints,IntCon,LB,UB,\
                 initpopulation,Elitecount,crossoverfraction,mutationrate,\
                 createfunction,crossoverfunction,mutationfunction,selectionfunction,fitnessscalingfunction,\
                 verbose,options):
@@ -13,6 +31,7 @@ class Candidates:
         self.options = options
         self.popsize = popsize
         self.chromesize = chromesize
+        self.targetsize = targetsize
         self.func = func
         self.constraints = constraints
         self.IntCon = IntCon
@@ -37,29 +56,101 @@ class Candidates:
         self.crossoverfraction = crossoverfraction
         self.mutationrate = mutationrate
 
-        
-
         self.createfunction = createfunction
         self.crossoverfunction = crossoverfunction
         self.mutationfunction = mutationfunction
         self.selectionfunction = selectionfunction
         self.fitnessscalingfunction = fitnessscalingfunction
-
-        self.fitness = np.zeros(popsize)
-        self.scaledfitness = np.zeros(popsize)
+    
+        self.fitness = np.zeros((popsize,targetsize))
+        self.scaledfitness = np.zeros((popsize,targetsize))
         self.fit()
+
+    def sort(self):
+        '''
+        Fast non-dominated sorting
+
+        Reference:
+            Deb K, Pratap A, Agarwal S, et al. 
+            A fast and elitist multiobjective genetic algorithm: NSGA-II[J]. 
+            IEEE transactions on evolutionary computation, 2002, 6(2): 182-197.
+
+        '''
+        self.fronts = list()
+        self.fronts.append([])  # First Front is Blank
+
+        # Get first front firstly
+        for i in range(self.popsize):
+            self.individuals[i].dominatedindividuals = list()
+            self.individuals[i].dominatecount = 0
+
+            for j in range(self.popsize):
+                if self.individuals[i].dominate(self.individuals[j]):
+                    self.individuals[i].dominatedindividuals.add(i)
+                elif self.individuals[j].dominate(self.individuals[i]):
+                    self.individuals[i].dominatecount += 1
+            if self.individuals[i].dominatecount == 0:
+                self.fronts[0].append(i)
+                self.individuals[i].rank = 0
+
+        # Get subsequent front
+        index = 0
+        while len(self.fronts[index]) > 0:
+            nextfront = list()
+            for i in range(self.fronts[index]):
+                for j in self.individuals[i].dominatedindividuals:
+                    self.individuals[j].dominatecount -= 1
+                    if self.individuals[j].dominatecount == 0:
+                        self.individuals[j].rank = index + 1
+                        nextfront.append(j)
+            index += 1
+            self.fronts.append(nextfront)
+    
+    def calcrowdingdistance(self):
+        if len(self.fronts) > 0:
+            for index in range(len(self.fronts)):
+                front = self.fronts[index]
+                frontnum = len(front)
+                if frontnum > 0:
+                    for m in front:
+                        self.individuals[m].crowdingdistance = 0
+
+                    # TODO need check
+                    for k in range(self.targetsize):
+                        objectives = [self.individuals[i].objectives[k] for i in front]
+                        sortindex = np.argsort(objectives)
+                        maxobjective = objectives[sortindex[frontnum-1]]
+                        minobjective = objectives[sortindex[0]]
+                        posmax = front[sortindex[frontnum-1]]
+                        posmin = front[sortindex[0]]
+                        
+                        self.individuals[posmax].crowdingdistance = maxobjective
+                        self.individuals[posmin].crowdingdistance = minobjective
+                        
+                        # TODO Check!
+                        for i in range(1,len(sortindex)-1):
+                            pos = front[sortindex[i]]
+                            left = front[sortindex[i-1]]
+                            right = front[sortindex[i+1]]
+                            self.individuals[pos].crowdingdistance = \
+                                (self.individuals[right].crowdingdistance-self.individuals[left].crowdingdistance)/(maxobjective-minobjective)
+
+    def crowding(self):
+        pass
 
     def fit(self):
         '''
         Calculation of Fitness
         '''
-        rawfitness = np.zeros(self.popsize)
+        rawfitness = np.zeros((self.targetsize,self.popsize))
         for i in range(self.popsize):
             rawfitness[i] = self.func(self.populations[i]) \
-                + self.constraints.fitness(self.populations[i])
+                + self.constraints.fitness(self.populations[i])*np.ones(self.targetsize)
             self.fitness[i] = self.func(self.populations[i])
+    
         # Sacle the raw fitness into scaled fitness
-        self.scaledfitness = self.fitnessscalingfunction(rawfitness,args=self.options.FitnessScale.args)
+        for i in range(self.targetsize):
+            self.scaledfitness[:,i] = self.fitnessscalingfunction(rawfitness[:,i].flatten(),args=self.options.FitnessScale.args)
 
     def crossover(self):
         '''
--- Husky/GA/__init__.py
+++ Husky/GA/__init__.py
@@ -1,7 +1,9 @@
 from .GA import GA
+from .MultiGA import MultiGA
 
 __version__ = '0.0.1'
 
 __all__ = [
-    'GA'
+    'GA',
+    'MultiGA'
 ]
